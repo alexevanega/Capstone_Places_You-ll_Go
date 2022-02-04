27aba815ae405c7c0ada197df09174d4d002bc2e
Alex Evanega <alexevanega@gmail.com>
2022-02-03 19:48:33 -0500

    A few things  add to css

--- capstone_react/cap-react/src/views/WWD.js
+++ capstone_react/cap-react/src/views/WWD.js
@@ -7,7 +7,7 @@ export default class WWD extends Component {
     render() {
         return (
             <div className='col-3'>
-                
+                <h1>Home Page</h1>
             </div>
         )
     }
